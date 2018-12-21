f2baf984dbdc68fae4395615208930f7285a0d31
José Simões <josesimoes@users.noreply.github.com>
2018-12-21 14:51:30 +0000

    Add Power class to manage target power mode (#16)

---
+++ source/nanoFramework.Hardware.Stm32/Power.cs
@@ -0,0 +1,29 @@
+﻿//
+// Copyright (c) 2018 The nanoFramework project contributors
+// See LICENSE file in the project root for full license information.
+//
+
+using System.Runtime.CompilerServices;
+
+namespace nanoFramework.Hardware.Stm32
+{
+    /// <summary>
+    /// Provides methods to control power mode of the target CPU.
+    /// </summary>
+    /// <remarks>
+    /// This API is available only for SMT32 targets.
+    /// </remarks>
+    public static class Power
+    {
+        #region native methods calls
+
+        /// <summary>
+        /// Sets the target device to enter SMT32 "standby" mode.
+        /// </summary>
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        public static extern void EnterStandbyMode();
+
+        #endregion
+
+    }
+}
