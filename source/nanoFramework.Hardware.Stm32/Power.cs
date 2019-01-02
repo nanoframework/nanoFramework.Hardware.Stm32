8fc13cd08abc95e73825ba3fff82d8e0ce5fc75b
José Simões <jose.simoes@eclo.solutions>
2019-01-02 15:58:22 +0000

    Add methods to enable/disable wake-up from pin
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

--- source/nanoFramework.Hardware.Stm32/Power.cs
+++ source/nanoFramework.Hardware.Stm32/Power.cs
@@ -3,6 +3,7 @@
 // See LICENSE file in the project root for full license information.
 //
 
+using System;
 using System.Runtime.CompilerServices;
 
 namespace nanoFramework.Hardware.Stm32
@@ -13,9 +14,30 @@ namespace nanoFramework.Hardware.Stm32
     /// <remarks>
     /// This API is available only for SMT32 targets.
     /// </remarks>
-    public static class Power
+    public static partial class Power
     {
+
         #region native methods calls
+
+        /// <summary>
+        /// Disables the specified <paramref name="pin"/> as wake-up source.
+        /// </summary>
+        /// <remarks>
+        /// The wake-up pin availability is target dependent. Check the target device data-sheet for details.
+        /// If the specified pin is not available an <see cref="ArgumentException"/> is  thrown.
+        /// </remarks>
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        public static extern void DisableWakeupPin(WakeupPin pin = WakeupPin.Pin1);
+
+        /// <summary>
+        /// Enables the specified <paramref name="pin"/> as wake-up source.
+        /// </summary>
+        /// <remarks>
+        /// The wake-up pin availability is target dependent. Check the target device data-sheet for details.
+        /// If the specified pin is not available an <see cref="ArgumentException"/> is  thrown.
+        /// </remarks>
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        public static extern void EnableWakeupPin(WakeupPin pin = WakeupPin.Pin1);
 
         /// <summary>
         /// Sets the target device to enter SMT32 "standby" mode.
