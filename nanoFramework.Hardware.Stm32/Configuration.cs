aaa9d49daa2f8da40289d28cfc5303828753f2b6
José Simões <jose.simoes@eclo.solutions>
2023-01-26 19:25:34 +0000

    Add methods to add and remove ADC channel definition

--- nanoFramework.Hardware.Stm32/Configuration.cs
+++ nanoFramework.Hardware.Stm32/Configuration.cs
@@ -3,6 +3,7 @@
 // See LICENSE file in the project root for full license information.
 //
 
+using System;
 using System.Runtime.CompilerServices;
 
 namespace nanoFramework.Hardware.Stm32
@@ -26,6 +27,45 @@ namespace nanoFramework.Hardware.Stm32
         public static extern void ConfigurePin(
             int pin,
             GpioConfiguration configuration);
+
+
+        /// <summary>
+        /// Add definition of an ADC channel.
+        /// </summary>
+        /// <param name="adc">Index of ADC block.</param>
+        /// <param name="channel">Channel number.</param>
+        /// <param name="pin">The pin number to connect to ADC channel.</param>
+        /// <exception cref="ArgumentException">
+        /// <para>
+        /// If the ADC block doesn't exist.
+        /// </para>
+        /// <para>
+        /// - or -
+        /// </para>
+        /// <para>
+        /// If the ADC channel is already assigned.
+        /// </para>
+        /// </exception>
+        /// <remarks>
+        /// No validation is performed on the usage of the GPIO pin. This will override any other pre-existing configuration for it.
+        /// </remarks>
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        public static extern void AddAdcChannel(
+            int adc,
+            int channel,
+            int pin);
+
+        /// <summary>
+        /// Removes an existing ADC channel definition.
+        /// </summary>
+        /// <param name="adc">Index of ADC block.</param>
+        /// <param name="channel">Channel number.</param>
+        /// <exception cref="ArgumentException">If the ADC definition for the combination <paramref name="adc"/> and <paramref name="channel"/> doesn't exist.</exception>
+        /// <remarks>Upon return the respective GPIO pin is configured to the default configuration: input, without any other activation.</remarks>
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        public static extern void RemoveAdcChannel(
+            int adc,
+            int channel);
 
 #pragma warning restore S4200 // Native methods should be wrapped
 
